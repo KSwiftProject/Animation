0795dc9e6897d5d166fbd4c0db2b06208b8ef941
Lee Khai <leekhai18@gmail.com>
2018-09-14 17:14:51 +0700

    include 4 animation using CGAffine

--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -23,6 +23,52 @@ class ViewController: UIViewController {
     }
     
     @IBAction func tapped(_ sender: Any) {
+        tap.isHidden = true
+        
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: [],
+                       animations: { [unowned self] in
+                        switch self.currentAnimation {
+                        case 0:
+                            self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
+                            break
+                            
+                        case 1:
+                            self.imageView.transform = CGAffineTransform.identity
+                            break
+                            
+                        case 2:
+                            self.imageView.transform = CGAffineTransform(translationX: -256, y: -256)
+                            break
+                            
+                        case 3:
+                            self.imageView.transform = CGAffineTransform.identity
+                            break
+                            
+                        case 4:
+                            self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+                            break
+                            
+                        case 5:
+                            self.imageView.transform = CGAffineTransform.identity
+                            break
+                            
+                        case 6:
+                            self.imageView.alpha = 0.1
+                            self.imageView.backgroundColor = UIColor.green
+                            break
+                            
+                        case 7:
+                            self.imageView.alpha = 1
+                            self.imageView.backgroundColor = UIColor.clear
+                            break
+                            
+                        default:
+                            break
+                        }
+        }) { [unowned self] (finished: Bool) in
+            self.tap.isHidden = false
+        }
+        
         currentAnimation += 1
         
         if currentAnimation > 7 {
